fb6269eeb1457a47a07494d8a036abcef229faa1
Ymberê Xavier <ymbere@gmail.com>
2024-10-30 14:29:22 -0300

    [FEATURE] - Add dropdown component (#11)
    * feat: add dropdown component
    
    * test: add unit tests
    
    * style: add generic custom style

---
+++ src/styles/common/index.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const BaseContainer = styled.div`
+    border: 1px solid rgba(209, 208, 217, 1);
+    padding: 11px 12px 11px 12px;
+    border-radius: 4px;
+`;
