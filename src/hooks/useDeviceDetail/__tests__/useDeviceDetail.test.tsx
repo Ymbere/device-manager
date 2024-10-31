5fb7806d96d3006bb1d38d1c609590ae11f84fd4
Ymbere Xavier <ymbere@gmail.com>
2024-10-30 23:28:28 -0300

    test: add unit test for useDeviceDetail

---
+++ src/hooks/useDeviceDetail/__tests__/useDeviceDetail.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { renderHook, waitFor } from '@testing-library/react';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {useDeviceDetail} from "../index";
+
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+test('Should return the device detail properly', async () => {
+  const queryClient = new QueryClient();
+
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+  )
+
+  const mockDeviceDetail = {
+    id: '1', system_name: 'Device 1', type: 'Type A', hdd_capacity: '500GB'
+  }
+
+  global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockDeviceDetail),
+      })
+  ) as jest.Mock;
+
+  const { result } = renderHook(() => useDeviceDetail('1'), { wrapper });
+
+  await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+  expect(result.current.data).toEqual(mockDeviceDetail);
+})
