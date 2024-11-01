ffecb36f4db16e0cdf8d67c0f73690471a7805f4
Ymbere Xavier <ymbere@gmail.com>
2024-11-01 20:59:48 -0300

    refactor: update modal component
    Move the styled components to a different folder, adjust unit tests

--- src/components/modal/__tests__/Modal.test.tsx
+++ src/components/modal/__tests__/Modal.test.tsx
@@ -1,57 +1,49 @@
 import React, {act} from 'react';
 import {render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
-
-
 import Modal from "../index";
 
 const baseProps = {
   modalTitle: 'My test modal',
-  onClose: () => {
-  },
+  onClose: () => {},
   actions: <>test</>,
   isOpen: true,
 }
 
-
-test('Should render the modal without crashing', () => {
-  render(
+describe('Modal Component', () => {
+  it('should render the modal without crashing', () => {
+    render(
       <Modal {...baseProps}>
-        <div>
-          Modal Children
-        </div>
+        <div>Modal Children</div>
       </Modal>
-  );
+    );
 
-  const modalChildren = screen.getByText('Modal Children');
+    const modalChildren = screen.getByText('Modal Children');
+    expect(modalChildren).toBeVisible();
+  });
 
-  expect(modalChildren).toBeVisible();
+  it('should call onClose when clicking on close button', async () => {
+    const user = userEvent.setup();
+    baseProps.onClose = jest.fn();
+
+    render(
+      <Modal {...baseProps}>
+        <div>Modal Children</div>
+      </Modal>
+    );
+
+    const modalChildren = screen.getByText('Modal Children');
+    expect(modalChildren).toBeVisible();
+
+    const modalCloseButton = screen.getByTestId('close-modal-button');
+
+    act(() => {
+      user.click(modalCloseButton);
+    });
+
+    await waitFor(() => {
+      expect(baseProps.onClose).toHaveBeenCalledTimes(1);
+    });
+  });
 });
-
-test('Should call onClose when clicking on close button', async () => {
-  const user = userEvent.setup();
-  baseProps.onClose = jest.fn();
-
-  render(
-      <Modal {...baseProps}>
-        <div>
-          Modal Children
-        </div>
-      </Modal>
-  );
-
-  const modalChildren = screen.getByText('Modal Children');
-
-  expect(modalChildren).toBeVisible();
-
-  const modalCloseButton = screen.getByTestId('close-modal-button');
-
-  act(() => {
-    user.click(modalCloseButton);
-  });
-
-  await waitFor(() => {
-    expect(baseProps.onClose).toHaveBeenCalledTimes(1);
-  });
-})
 
---
+++ src/components/modal/styles/index.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const StyledOverlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 0;
+`;
+
+export const StyledModalContainer = styled.div`
+    padding: 24px;
+    background: white;
+    border-radius: 4px;
+    max-width: 540px;
+    max-height: 80vh;
+    overflow-y: auto;
+    z-index: 1;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+`;
+
+export const StyledModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 24px;
+  & > span:first-of-type {
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 29.05px;
+    text-align: left;
+  }
+`;
+
+export const StyledModalFooter = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    gap: 8px;
+`;
+
+export const StyledModalContent = styled.div`
+  margin-bottom: 32px;
+`;
