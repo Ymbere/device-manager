f8bed2312a0adce72f721ee25a56d84ec3f6af30
Ymberê Xavier <ymbere@gmail.com>
2024-11-01 16:44:08 -0300

    [FEATURE] - Add header component for NinjaOne (#35)
    * feat: add component for ninjaOne header
    
    * style: add styles for header
    
    * test: add unit test

---
+++ src/components/ninjaHeader/index.tsx
@@ -0,0 +1,11 @@
+import { Header, Logo } from "./styles";
+
+const NinjaHeader = () => {
+    return (
+        <Header data-testid="ninja-header">
+            <Logo />
+        </Header>
+    );
+};
+
+export default NinjaHeader;
