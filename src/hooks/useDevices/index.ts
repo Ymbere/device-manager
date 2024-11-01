f44166f5c8f8d14938473858af0a5d98e10570f0
Ymbere Xavier <ymbere@gmail.com>
2024-11-01 20:47:58 -0300

    refactor: update useDevices
    update useDevices to return hdd_capacity as a number

--- src/hooks/useDevices/index.ts
+++ src/hooks/useDevices/index.ts
@@ -5,15 +5,20 @@ export interface Device {
   id: string,
   system_name: string,
   type: string,
-  hdd_capacity: string,
+  hdd_capacity: number,
 }
 
 const fetchDevices = async (): Promise<Array<Device>> => {
   const url = `${BASE_API_URL}/devices`;
   const response = await fetch(url);
 
-  return await response.json();
-};
+  const devices:Device[] = await response.json();
+
+  return devices.map(device => ({
+    ...device,
+    hdd_capacity: Number(device.hdd_capacity)
+  }));
+}
 
 export const useDevices = () => {
   return useQuery<Array<Device>, Error>({
