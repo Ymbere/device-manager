392a7aa92497b4a13b3a58818ef97e2a28d44c17
Ymbere Xavier <ymbere@gmail.com>
2024-11-01 16:58:31 -0300

    refactor: adjust styles for the text inpout
    Added property for the width style and also moved styled component to
    different folder.

--- src/components/searchInput/index.tsx
+++ src/components/searchInput/index.tsx
@@ -1,39 +1,25 @@
 import React, { InputHTMLAttributes } from 'react';
-import styled from 'styled-components';
-import { BaseContainer } from "../../styles/common";
+import { StyledTextBoxContainer, StyledTextBoxInput } from './styles';
 
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  placeholder: string;
+  placeholder?: string;
   icon?: React.ReactNode;
+  width?: string;
 }
 
-const TextBoxContainer = styled(BaseContainer)`
-    display: flex;
-`
-
-const TextBoxInput = styled.input`
-    width: 100%;
-    border: none;
-    padding: 0;
-    box-sizing: border-box;
-    margin-left: 8px;
-
-    :focus {
-        outline: none;
-        box-shadow: none;
-        border-color: transparent;
-    }
-`;
-
-const TextInput = ({ icon, ...props }: TextInputProps) => {
+const TextInput = ({ icon, width, ...props }: TextInputProps) => {
+  const hasIcon = !!icon;
 
   return (
-    <TextBoxContainer>
+    <StyledTextBoxContainer width={width}>
       {icon}
-      <TextBoxInput data-testid='searchbox-input' {...props} />
-    </TextBoxContainer>
-  )
-
-}
+      <StyledTextBoxInput
+        data-testid='searchbox-input'
+        $hasIcon={hasIcon}
+        {...props}
+      />
+    </StyledTextBoxContainer>
+  );
+};
 
 export default TextInput;
