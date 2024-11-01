345b03d199ab03595065cc1e8dd032744504ab29
Ymbere Xavier <ymbere@gmail.com>
2024-11-01 20:10:50 -0300

    refactor: update button
    Move button style to different folder, adjust unit tests

--- src/components/button/__tests__/Button.test.tsx
+++ src/components/button/__tests__/Button.test.tsx
@@ -2,32 +2,34 @@ import React, { act } from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import Button, {AddButtonProps} from "../index";
+import Button, { ButtonProps } from "../index";
 
-const baseProps: AddButtonProps = {
+const baseProps: ButtonProps = {
   buttonLabel: 'Test button',
   onClick: () => {},
   buttonType: "primary"
 }
 
-test('Should render the button without crashing', () => {
-  render(<Button {...baseProps} />);
-  const button = screen.getByText(baseProps.buttonLabel);
-  expect(button).toBeVisible();
-});
-
-test('Should be able to click on the button', async () => {
-  baseProps.onClick = jest.fn();
-  const user = userEvent.setup();
-  render(<Button {...baseProps} />);
-  const button = screen.getByText(baseProps.buttonLabel);
-  expect(button).toBeVisible();
-
-  act(() => {
-    user.click(button);
+describe('Button component', () => {
+  it('renders the button without crashing', () => {
+    render(<Button {...baseProps} />);
+    const button = screen.getByText(baseProps.buttonLabel);
+    expect(button).toBeVisible();
   });
 
-  await waitFor(() => {
-    expect(baseProps.onClick).toHaveBeenCalledTimes(1);
+  it('calls onClick when the button is clicked', async () => {
+    baseProps.onClick = jest.fn();
+    const user = userEvent.setup();
+    render(<Button {...baseProps} />);
+    const button = screen.getByText(baseProps.buttonLabel);
+    expect(button).toBeVisible();
+
+    act(() => {
+      user.click(button);
+    });
+
+    await waitFor(() => {
+      expect(baseProps.onClick).toHaveBeenCalledTimes(1);
+    });
   });
 });
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,52 +1,20 @@
-import styled, { css } from "styled-components";
+import { StyledButton } from './styles';
 
-export interface AddButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   buttonLabel: string;
   iconLeft?: React.ReactNode;
   buttonType: 'primary' | 'secondary' | 'danger' | 'accessory';
 }
 
-const ButtonContainer = styled.button<{ buttonType: 'primary' | 'secondary' | 'danger' | 'accessory' }>`
-    border: 1px solid transparent;
-    border-radius: 4px;
-    padding: 12px;
-    display: flex;
-    justify-content: space-between;
-    gap: 8px;
-    color: white;
-
-    ${({ buttonType }) => buttonType === 'primary' && css`
-        background: rgba(51, 122, 183, 1);
-        color: rgba(255, 255, 255, 1);
-    `}
-
-    ${({ buttonType }) => buttonType === 'secondary' && css`
-        background: rgba(255, 255, 255, 1);
-        color: rgba(51, 122, 183, 1);
-        border: 1px solid rgba(72, 68, 105, 0.25);
-    `}
-
-    ${({ buttonType }) => buttonType === 'danger' && css`
-        background: rgba(213, 57, 72, 1);
-        color: rgba(255, 255, 255, 1);
-    `}
-
-    ${({ buttonType }) => buttonType === 'accessory' && css`
-        background: rgba(255, 255, 255, 1);
-        color: rgba(33, 31, 51, 1);
-        border: 1px solid rgba(72, 68, 105, 0.25);
-    `}
-`
-
-const Button = ({ buttonLabel, buttonType, onClick, iconLeft }: AddButtonProps) => {
+const Button = ({ buttonLabel, buttonType, onClick, iconLeft }: ButtonProps) => {
   return (
-      <ButtonContainer
+      <StyledButton
           onClick={onClick}
-          buttonType={buttonType}
+          $buttonType={buttonType}
       >
         {iconLeft}
         {buttonLabel}
-      </ButtonContainer>
+      </StyledButton>
   )
 }
 
