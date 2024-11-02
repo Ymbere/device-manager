03aa7d1e6e93f142a46effe2e6e2c1c975c8c94d
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 03:15:21 -0300

    chore: prettier write on deviceTable

--- src/pages/initial/components/deviceTable/DeviceTable.tsx
+++ src/pages/initial/components/deviceTable/DeviceTable.tsx
@@ -4,11 +4,9 @@ import Table from '../../../../components/table';
 import { useDeviceContext } from '../../../../context/DeviceContext';
 
 const DeviceTable = () => {
-    const { tableRows } = useDeviceContext();
+  const { tableRows } = useDeviceContext();
 
-    return (
-        <Table headerTitle='Devices' tableData={tableRows} />
-    );
+  return <Table headerTitle="Devices" tableData={tableRows} />;
 };
 
 export default DeviceTable;
