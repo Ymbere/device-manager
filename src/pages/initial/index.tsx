9c651b41a834753ddf8672107cffbdf3d7cc5cf8
Ymberê Xavier <ymbere@gmail.com>
2024-11-02 15:20:30 -0300

    [FEATURE] - Adjust styled component (#55)
    * style: adjust styled component
    
    * chore: add package-lock.json
    
    * chore: remove default unit test

--- src/pages/initial/index.tsx
+++ src/pages/initial/index.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import DeviceTable from './components/deviceTable/DeviceTable';
 import DeviceFilters from './components/devicesFilter/DevicesFilter';
 import DevicesHeader from './components/devicesHeader/DevicesHeader';
-import styled from 'styled-components';
+import { StyledPage } from './styles';
 
-const StyledPage = styled.div`
-  padding: 24px 24px 0 24px;
-`;
 
 const InitialPage = () => {
   return (
