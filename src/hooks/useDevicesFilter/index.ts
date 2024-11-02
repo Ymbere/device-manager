1df00bdd5f5e6feb12b7678eff6a4d4c738b7b12
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 03:14:36 -0300

    chore: prettier write on useDevicesFilter

--- src/hooks/useDevicesFilter/index.ts
+++ src/hooks/useDevicesFilter/index.ts
@@ -1,120 +1,123 @@
-import React, { useReducer, useCallback, useMemo } from 'react';
+import React from 'react';
 import { DropdownItem } from '../../components/dropdown';
 import { Device } from '../useDevices';
 
 interface SetDeviceTypeFilterAction {
-    type: 'SET_DEVICE_TYPE_FILTER';
-    payload: string[];
+  type: 'SET_DEVICE_TYPE_FILTER';
+  payload: string[];
 }
 
 interface SetDeviceSortingFilterAction {
-    type: 'SET_DEVICE_SORTING_FILTER';
-    payload: string[];
+  type: 'SET_DEVICE_SORTING_FILTER';
+  payload: string[];
 }
 
 interface SetDeviceSearchFilterAction {
-    type: 'SET_DEVICE_SEARCH_FILTER';
-    payload: string;
+  type: 'SET_DEVICE_SEARCH_FILTER';
+  payload: string;
 }
 
-interface ClearFiltersAction{
-    type: 'CLEAR_FILTERS';
+interface ClearFiltersAction {
+  type: 'CLEAR_FILTERS';
 }
 
 type FiltersAction =
-    | SetDeviceTypeFilterAction
-    | SetDeviceSortingFilterAction
-    | SetDeviceSearchFilterAction
-    | ClearFiltersAction;
+  | SetDeviceTypeFilterAction
+  | SetDeviceSortingFilterAction
+  | SetDeviceSearchFilterAction
+  | ClearFiltersAction;
 
 export interface FiltersState {
-    deviceTypeFilter: string[];
-    deviceSortingFilter: string[];
-    deviceSearchFilter: string;
+  deviceTypeFilter: string[];
+  deviceSortingFilter: string[];
+  deviceSearchFilter: string;
 }
 
 const initialState: FiltersState = {
-    deviceTypeFilter: [],
-    deviceSortingFilter: [],
-    deviceSearchFilter: ''
+  deviceTypeFilter: [],
+  deviceSortingFilter: [],
+  deviceSearchFilter: '',
 };
 
 const filtersReducer = (state: FiltersState, action: FiltersAction): FiltersState => {
-    switch (action.type) {
-        case 'SET_DEVICE_TYPE_FILTER':
-            return { ...state, deviceTypeFilter: action.payload };
-        case 'SET_DEVICE_SORTING_FILTER':
-            return { ...state, deviceSortingFilter: action.payload };
-        case 'SET_DEVICE_SEARCH_FILTER':
-            return { ...state, deviceSearchFilter: action.payload };
-        case 'CLEAR_FILTERS':
-            return initialState;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'SET_DEVICE_TYPE_FILTER':
+      return { ...state, deviceTypeFilter: action.payload };
+    case 'SET_DEVICE_SORTING_FILTER':
+      return { ...state, deviceSortingFilter: action.payload };
+    case 'SET_DEVICE_SEARCH_FILTER':
+      return { ...state, deviceSearchFilter: action.payload };
+    case 'CLEAR_FILTERS':
+      return initialState;
+    default:
+      return state;
+  }
 };
 
 export const useDeviceFilters = (devices: Device[]) => {
-    const [filters, dispatch] = useReducer(filtersReducer, initialState);
+  const [filters, dispatch] = React.useReducer(filtersReducer, initialState);
 
-    const handleChangeDeviceTypeFilter = useCallback((selectedValues: DropdownItem | DropdownItem[]) => {
-        if (Array.isArray(selectedValues)) {
-            const newSelectedTypes = selectedValues.map((selectedValue) => selectedValue.value);
-            dispatch({ type: 'SET_DEVICE_TYPE_FILTER', payload: newSelectedTypes });
-        }
-    }, []);
+  const handleChangeDeviceTypeFilter = React.useCallback((selectedValues: DropdownItem | DropdownItem[]) => {
+    if (Array.isArray(selectedValues)) {
+      const newSelectedTypes = selectedValues.map((selectedValue) => selectedValue.value);
+      dispatch({ type: 'SET_DEVICE_TYPE_FILTER', payload: newSelectedTypes });
+    }
+  }, []);
 
-    const handleChangeDeviceSortingFilter = useCallback((selectedValue: DropdownItem | DropdownItem[]) => {
-        if (!Array.isArray(selectedValue)) {
-            dispatch({ type: 'SET_DEVICE_SORTING_FILTER', payload: [selectedValue.value] });
-        }
-    }, []);
+  const handleChangeDeviceSortingFilter = React.useCallback((selectedValue: DropdownItem | DropdownItem[]) => {
+    if (!Array.isArray(selectedValue)) {
+      dispatch({
+        type: 'SET_DEVICE_SORTING_FILTER',
+        payload: [selectedValue.value],
+      });
+    }
+  }, []);
 
-    const handleSearchFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        dispatch({ type: 'SET_DEVICE_SEARCH_FILTER', payload: event.target.value });
-    };
+  const handleSearchFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: 'SET_DEVICE_SEARCH_FILTER', payload: event.target.value });
+  };
 
-    const clearFilters = () => {
-        dispatch({ type: 'CLEAR_FILTERS' });
-    };
+  const clearFilters = () => {
+    dispatch({ type: 'CLEAR_FILTERS' });
+  };
 
-    const filteredDevicesList = useMemo(() => {
-        let updatedDevicesList = devices ? [...devices] : [];
+  const filteredDevicesList = React.useMemo(() => {
+    let updatedDevicesList = devices ? [...devices] : [];
 
-        if (filters.deviceSearchFilter.length > 0) {
-            updatedDevicesList = updatedDevicesList.filter(device =>
-                device.system_name.toLowerCase().includes(filters.deviceSearchFilter.toLowerCase())
-            );
-        }
+    if (filters.deviceSearchFilter.length > 0) {
+      updatedDevicesList = updatedDevicesList.filter((device) =>
+        device.system_name.toLowerCase().includes(filters.deviceSearchFilter.toLowerCase())
+      );
+    }
 
-        if (filters.deviceTypeFilter.length > 0) {
-            updatedDevicesList = updatedDevicesList.filter((device) => filters.deviceTypeFilter.includes(device.type));
-        }
+    if (filters.deviceTypeFilter.length > 0) {
+      updatedDevicesList = updatedDevicesList.filter((device) => filters.deviceTypeFilter.includes(device.type));
+    }
 
-        if (filters.deviceSortingFilter.length > 0) {
-            const sortTerms = filters.deviceSortingFilter[0].split('-')
-            const [key, order] = sortTerms;
+    if (filters.deviceSortingFilter.length > 0) {
+      const sortTerms = filters.deviceSortingFilter[0].split('-');
+      const [key, order] = sortTerms;
 
-            if (key === 'system_name') {
-                updatedDevicesList.sort((a, b) => a[key].localeCompare(b[key]));
-            } else if (key === 'hdd_capacity') {
-                updatedDevicesList.sort((a, b) => a[key] - b[key]);
-            }
+      if (key === 'system_name') {
+        updatedDevicesList.sort((a, b) => a[key].localeCompare(b[key]));
+      } else if (key === 'hdd_capacity') {
+        updatedDevicesList.sort((a, b) => a[key] - b[key]);
+      }
 
-            if (order === 'DSC') {
-                updatedDevicesList.reverse();
-            }
-        }
+      if (order === 'DSC') {
+        updatedDevicesList.reverse();
+      }
+    }
 
-        return updatedDevicesList;
-    }, [devices, filters]);
+    return updatedDevicesList;
+  }, [devices, filters]);
 
-    return {
-        filters,
-        handleChangeDeviceTypeFilter,
-        handleChangeDeviceSortingFilter,
-        handleSearchFilterChange,
-        clearFilters,
-        filteredDevicesList
-    };
+  return {
+    filters,
+    handleChangeDeviceTypeFilter,
+    handleChangeDeviceSortingFilter,
+    handleSearchFilterChange,
+    clearFilters,
+    filteredDevicesList,
+  };
 };
