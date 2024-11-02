18841cede087e411903bf0f22661804f2c42d06e
Ymbere Xavier <ymbere@gmail.com>
2024-11-01 22:07:08 -0300

    feat: add device table wrapper
    A component to wrap the device table

---
+++ src/pages/initial/components/deviceTable/DeviceTable.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Table from '../../../../components/table';
+import { useDeviceContext } from '../../../../context/DeviceContext';
+
+const DeviceTable = () => {
+    const { tableRows } = useDeviceContext();
+
+    return (
+        <Table headerTitle='Devices' tableData={tableRows} />
+    );
+};
+
+export default DeviceTable;
