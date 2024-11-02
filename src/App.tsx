ccbaee2c027773c461ba2f275418113ba01263c6
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 03:17:24 -0300

    chore: prettier write on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import InitialPage from "./pages/initial";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import InitialPage from './pages/initial';
 import { DeviceProvider } from './context/DeviceContext';
 import NinjaHeader from './components/ninjaHeader';
 import { ThemeProvider } from 'styled-components';
