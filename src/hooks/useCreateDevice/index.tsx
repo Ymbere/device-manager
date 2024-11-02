ab9ae283c19de372c12b100dd0391c9c6588b11e
Ymbere Xavier <ymbere@gmail.com>
2024-11-01 21:09:33 -0300

    refactor: update useCreateDevice
    Update hook to adjust property of mutation and adjust unit test

--- src/hooks/useCreateDevice/index.tsx
+++ src/hooks/useCreateDevice/index.tsx
@@ -1,8 +1,8 @@
 import {useMutation, useQueryClient} from '@tanstack/react-query';
-import {Device} from "../useDevices";
+import { DevicePayload } from '../useDeviceUpdate';
 import { BASE_API_URL } from '../../consts';
 
-const createDevice = async (device: Omit<Device, 'id'>): Promise<void> => {
+const createDevice = async (device: Omit<DevicePayload, 'id'>): Promise<void> => {
   const url = `${BASE_API_URL}/devices`;
   const response = await fetch(url, {
     method: 'POST',
@@ -17,7 +17,7 @@ export const useCreateDevice = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (device: Omit<Device, 'id'>) => createDevice(device),
+    mutationFn: (device: Omit<DevicePayload, 'id'>) => createDevice(device),
     onSuccess: () => {
       queryClient.invalidateQueries({queryKey: ['devices']})
     }
