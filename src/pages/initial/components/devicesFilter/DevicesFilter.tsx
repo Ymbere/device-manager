75f3513091a7f5b7c5078d7d67110b5e7c3732e6
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 03:15:36 -0300

    chore: prettier write on devicesFilter

--- src/pages/initial/components/devicesFilter/DevicesFilter.tsx
+++ src/pages/initial/components/devicesFilter/DevicesFilter.tsx
@@ -7,55 +7,53 @@ import { useDeviceContext } from '../../../../context/DeviceContext';
 import { DevicesFilterContainer, StyledReloadIcon, StyledFilters } from './styles';
 
 const DeviceFilters = () => {
-    const {
-        filters,
-        handleSearchFilterChange,
-        handleChangeDeviceTypeFilter,
-        handleChangeDeviceSortingFilter,
-        clearFilters,
-    } = useDeviceContext();
+  const {
+    filters,
+    handleSearchFilterChange,
+    handleChangeDeviceTypeFilter,
+    handleChangeDeviceSortingFilter,
+    clearFilters,
+  } = useDeviceContext();
 
-    return (
-        <DevicesFilterContainer>
-            <StyledFilters>
-                <SearchInput
-                    placeholder="Search"
-                    onChange={handleSearchFilterChange}
-                    value={filters.deviceSearchFilter}
-                    icon={<SearchIcon />}
-                    width="270px"
-                />
-                <Dropdown
-                    dropdownOptions={[
-                        { value: DEVICE_TYPES.MAC, text: 'Mac Os' },
-                        { value: DEVICE_TYPES.WINDOWS, text: 'Windows' },
-                        { value: DEVICE_TYPES.LINUX, text: 'Linux' }
-                    ]}
-                    multiple
-                    infoLabel="Device Type: "
-                    onChange={handleChangeDeviceTypeFilter}
-                    selectedValues={filters.deviceTypeFilter}
-                />
-                <Dropdown
-                    dropdownOptions={[
-                        { value: 'hdd_capacity-ASC', text: 'HDD Capacity (Ascending)' },
-                        { value: 'hdd_capacity-DSC', text: 'HDD Capacity (Descending)' },
-                        { value: 'system_name-ASC', text: 'Name (Ascending)' },
-                        { value: 'system_name-DSC', text: 'Name (Descending)' }
-                    ]}
-                    infoLabel="Sort by: "
-                    onChange={handleChangeDeviceSortingFilter}
-                    selectedValues={filters.deviceSortingFilter}
-                />
-            </StyledFilters>
+  return (
+    <DevicesFilterContainer>
+      <StyledFilters>
+        <SearchInput
+          placeholder="Search"
+          onChange={handleSearchFilterChange}
+          value={filters.deviceSearchFilter}
+          icon={<SearchIcon />}
+          width="270px"
+        />
+        <Dropdown
+          dropdownOptions={[
+            { value: DEVICE_TYPES.MAC, text: 'Mac Os' },
+            { value: DEVICE_TYPES.WINDOWS, text: 'Windows' },
+            { value: DEVICE_TYPES.LINUX, text: 'Linux' },
+          ]}
+          multiple
+          infoLabel="Device Type: "
+          onChange={handleChangeDeviceTypeFilter}
+          selectedValues={filters.deviceTypeFilter}
+        />
+        <Dropdown
+          dropdownOptions={[
+            { value: 'hdd_capacity-ASC', text: 'HDD Capacity (Ascending)' },
+            { value: 'hdd_capacity-DSC', text: 'HDD Capacity (Descending)' },
+            { value: 'system_name-ASC', text: 'Name (Ascending)' },
+            { value: 'system_name-DSC', text: 'Name (Descending)' },
+          ]}
+          infoLabel="Sort by: "
+          onChange={handleChangeDeviceSortingFilter}
+          selectedValues={filters.deviceSortingFilter}
+        />
+      </StyledFilters>
 
-
-            <StyledReloadIcon onClick={clearFilters}>
-                <ReloadIcon />
-            </StyledReloadIcon>
-
-        </DevicesFilterContainer>
-    );
+      <StyledReloadIcon onClick={clearFilters}>
+        <ReloadIcon />
+      </StyledReloadIcon>
+    </DevicesFilterContainer>
+  );
 };
 
 export default DeviceFilters;
--- src/pages/initial/components/devicesFilter/styles/index.tsx
+++ src/pages/initial/components/devicesFilter/styles/index.tsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 
 export const DevicesFilterContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export const StyledFilters = styled.div`
-display: flex;
-gap: ${({ theme }) => theme.spacing.medium};
-`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing.medium};
+`;
 
 export const StyledReloadIcon = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    cursor: pointer;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  cursor: pointer;
+`;
