b736f7a3c3629e6bc04630207145e0b0874b22e2
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 18:05:11 -0300

    refactor: improve the validation for form hook

--- src/hooks/useDeviceForm/index.tsx
+++ src/hooks/useDeviceForm/index.tsx
@@ -3,10 +3,64 @@ import { useDeviceDetail } from '../useDeviceDetail';
 import { useDeviceUpdate } from '../useDeviceUpdate';
 import { useCreateDevice } from '../useCreateDevice';
 
-const initialFormState = {
+interface validationRulesInterface {
+  [key: string]: {
+    validate: Array<(value: string) => string | boolean>;
+  };
+}
+
+export interface FormErrors {
+  system_name: string;
+  type: string;
+  hdd_capacity: string;
+}
+
+export const initialFormState = {
   system_name: '',
   type: '',
   hdd_capacity: '',
+};
+
+const validationRules: validationRulesInterface = {
+  'system_name': {
+    validate: [(value: string) => value.length > 0 || 'System name is required.'],
+  },
+  'type': {
+    validate: [(value: string) => value.length > 0 || 'Type is required.'],
+  },
+  'hdd_capacity': {
+    validate: [(value: string) => value.length > 0 || 'HDD capacity is required.'],
+  },
+};
+
+const validateField = (fieldName: string, fieldValue: string) => {
+  const rules = validationRules[fieldName];
+  if (rules) {
+    const errors = rules.validate
+      .map((validateFn) => validateFn(fieldValue))
+      .filter((result: string | boolean) => result !== true);
+
+    return errors.join(', ');
+  }
+  return '';
+};
+
+const validateForm = (formState: typeof initialFormState): FormErrors => {
+  const newErrors: FormErrors = {
+    system_name: '',
+    type: '',
+    hdd_capacity: '',
+  };
+
+  Object.keys(formState).forEach((formFieldKey) => {
+    const currentFormFieldValue = formState[formFieldKey as keyof typeof formState];
+    const error = validateField(formFieldKey, currentFormFieldValue);
+    if (error) {
+      newErrors[formFieldKey as keyof typeof newErrors] = error;
+    }
+  });
+
+  return newErrors;
 };
 
 export const useDeviceForm = (deviceId?: string | null) => {
@@ -19,6 +73,51 @@ export const useDeviceForm = (deviceId?: string | null) => {
 
   const [formState, setFormState] = React.useState(initialFormState);
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleDropdownChange = (selectedValues: any) => {
+    const value = !Array.isArray(selectedValues) ? selectedValues.value : '';
+    setFormState((prevState) => ({
+      ...prevState,
+      type: value,
+    }));
+  };
+
+  const clearForm = () => {
+    setFormState(initialFormState);
+  }
+
+  const mutate = async () => {
+    if (deviceId) {
+      return await updateDevice({ id: deviceId, ...formState });
+    } else {
+      return await createDevice(formState);
+    }
+  }
+
+  const handleSubmit = async () => {
+    const errors = validateForm(formState);
+
+    const hasErrors = Object.values(errors).some(error => error.length > 0);
+
+    if (hasErrors) {
+      return { status: 'formInvalid', errors };
+    }
+
+    try {
+      await mutate();
+      return { status: 'success' };
+    } catch (error: any) {
+      return { status: 'error', message: error.message };
+    }
+  };
+
   React.useEffect(() => {
     if (deviceDetail) {
       setFormState({
@@ -29,44 +128,9 @@ export const useDeviceForm = (deviceId?: string | null) => {
     }
   }, [deviceDetail]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormState((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleDropdownChange = (selectedValues: any) => {
-    setFormState((prevState) => ({
-      ...prevState,
-      type: !Array.isArray(selectedValues) ? selectedValues.value : '',
-    }));
-  };
-
-  const handleSubmit = async () => {
-    const { system_name, type, hdd_capacity } = formState;
-    if (!system_name || !type || !hdd_capacity) {
-      return {
-        status: 'error',
-        message: 'Please fill in all required fields.',
-      };
-    }
-    try {
-      if (deviceId) {
-        await updateDevice({ id: deviceId, ...formState });
-      } else {
-        await createDevice(formState);
-      }
-      setFormState(initialFormState);
-      return { status: 'success' };
-    } catch (error: any) {
-      return { status: 'error', message: error.message };
-    }
-  };
-
   return {
     formState,
+    clearForm,
     handleChange,
     handleDropdownChange,
     handleSubmit,
