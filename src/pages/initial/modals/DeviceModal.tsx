7653c56ac5d4267e8d28cde06f9646c73341a1c0
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 18:05:31 -0300

    refactor: handle the form error state

--- src/pages/initial/modals/DeviceModal.tsx
+++ src/pages/initial/modals/DeviceModal.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import Modal from '../../../components/modal';
 import Button from '../../../components/button';
 import DeviceForm from '../../../components/deviceForm';
-import { useDeviceForm } from '../../../hooks/useDeviceForm';
+import { FormErrors, useDeviceForm } from '../../../hooks/useDeviceForm';
 import { ModalButtonText, ModalHeaderText } from '../../../styles/common';
 
 interface DeviceModalProps {
@@ -10,17 +11,49 @@ interface DeviceModalProps {
   onClose: () => void;
 }
 
+const formErrorsInitialState: FormErrors = {
+  system_name: '',
+  type: '',
+  hdd_capacity: '',
+};
+
 const DeviceModal = ({ isOpen, deviceId, onClose }: DeviceModalProps) => {
-  const { formState, handleChange, handleDropdownChange, handleSubmit } = useDeviceForm(isOpen ? deviceId : null);
+  const { formState, handleChange, handleDropdownChange, handleSubmit, clearForm } = useDeviceForm(isOpen ? deviceId : null);
+  const [formErrors, setFormErrors] = React.useState<FormErrors>(formErrorsInitialState);
+
+  const handleOnClose = () => {
+    setFormErrors(formErrorsInitialState);
+    clearForm();
+    onClose();
+  }
 
   const handleFormSubmit = async () => {
     const result = await handleSubmit();
-    if (result.status === 'success') {
-      onClose();
+    if (result?.status === 'success') {
+      setFormErrors(formErrorsInitialState);
+      handleOnClose();
+    } else if (result?.status === 'formInvalid') {
+      setFormErrors(result.errors || formErrorsInitialState);
     } else {
       alert(result.message);
     }
   };
+
+  const handleFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [event.target.name]: '',
+    }));
+    handleChange(event);
+  };
+
+  const handleChangeDropdown = (selectedValue: any) => {
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      type: '',
+    }));
+    handleDropdownChange(selectedValue);
+  }
 
   return (
     <Modal
@@ -28,10 +61,10 @@ const DeviceModal = ({ isOpen, deviceId, onClose }: DeviceModalProps) => {
         deviceId ? <ModalHeaderText>Edit device</ModalHeaderText> : <ModalHeaderText> Create device</ModalHeaderText>
       }
       isOpen={isOpen}
-      onClose={onClose}
+      onClose={handleOnClose}
       actions={
         <>
-          <Button buttonLabel={<ModalButtonText>Cancel</ModalButtonText>} onClick={onClose} buttonType="secondary" />
+          <Button buttonLabel={<ModalButtonText>Cancel</ModalButtonText>} onClick={handleOnClose} buttonType="secondary" />
           <Button
             buttonLabel={<ModalButtonText>Submit</ModalButtonText>}
             onClick={handleFormSubmit}
@@ -40,7 +73,7 @@ const DeviceModal = ({ isOpen, deviceId, onClose }: DeviceModalProps) => {
         </>
       }
     >
-      <DeviceForm formState={formState} handleChange={handleChange} handleDropdownChange={handleDropdownChange} />
+      <DeviceForm formState={formState} formErrors={formErrors} handleChange={handleFieldChange} handleDropdownChange={handleChangeDropdown} />
     </Modal>
   );
 };
