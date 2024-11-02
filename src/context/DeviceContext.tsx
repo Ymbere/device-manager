d10e539733101dbe72ec3fb04b2e44faccbbe010
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 03:11:18 -0300

    chore: prettier write on device context

--- src/context/DeviceContext.tsx
+++ src/context/DeviceContext.tsx
@@ -6,60 +6,59 @@ import { TableRowData } from '../components/table';
 import { DropdownItem } from '../components/dropdown';
 
 interface DeviceContextType {
-    tableRows: TableRowData[];
-    filters: FiltersState;
-    handleSearchFilterChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    handleChangeDeviceSortingFilter: (selectedValue: DropdownItem | DropdownItem[]) => void;
-    handleChangeDeviceTypeFilter: (selectedValues: DropdownItem | DropdownItem[]) => void;
-    clearFilters: () => void;
+  tableRows: TableRowData[];
+  filters: FiltersState;
+  handleSearchFilterChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChangeDeviceSortingFilter: (selectedValue: DropdownItem | DropdownItem[]) => void;
+  handleChangeDeviceTypeFilter: (selectedValues: DropdownItem | DropdownItem[]) => void;
+  clearFilters: () => void;
 }
 
 const DeviceContext = React.createContext<DeviceContextType | undefined>(undefined);
 
 interface DeviceProviderProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const DeviceProvider = ({ children }: DeviceProviderProps) => {
-    const { data: devices, refetch } = useDevices();
+  const { data: devices, refetch } = useDevices();
 
-    const {
-        filteredDevicesList,
+  const {
+    filteredDevicesList,
+    filters,
+    handleSearchFilterChange,
+    handleChangeDeviceSortingFilter,
+    handleChangeDeviceTypeFilter,
+    clearFilters: clearDeviceFilters,
+  } = useDeviceFilters(devices ?? []);
+
+  const tableRows = useTableRows(filteredDevicesList);
+
+  const clearFilters = () => {
+    clearDeviceFilters();
+    refetch();
+  };
+
+  return (
+    <DeviceContext.Provider
+      value={{
+        tableRows,
         filters,
         handleSearchFilterChange,
         handleChangeDeviceSortingFilter,
         handleChangeDeviceTypeFilter,
-        clearFilters: clearDeviceFilters,
-    } = useDeviceFilters(devices ?? []);
-
-    const tableRows = useTableRows(filteredDevicesList);
-
-    const clearFilters = () => {
-        clearDeviceFilters();
-        refetch();
-    };
-
-    return (
-        <DeviceContext.Provider
-            value={{
-                tableRows,
-                filters,
-                handleSearchFilterChange,
-                handleChangeDeviceSortingFilter,
-                handleChangeDeviceTypeFilter,
-                clearFilters,
-            }}
-        >
-            {children}
-        </DeviceContext.Provider>
-    );
-
-}
+        clearFilters,
+      }}
+    >
+      {children}
+    </DeviceContext.Provider>
+  );
+};
 
 export const useDeviceContext = () => {
-    const context = React.useContext(DeviceContext);
-    if (context === undefined) {
-        throw new Error('useDeviceContext must be used within a DeviceProvider');
-    }
-    return context;
-}
+  const context = React.useContext(DeviceContext);
+  if (context === undefined) {
+    throw new Error('useDeviceContext must be used within a DeviceProvider');
+  }
+  return context;
+};
