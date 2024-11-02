6bf06ef9ec8eb1b1167dad7eb01de2b8572183de
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 03:08:46 -0300

    chore: prettier write on text input

--- src/components/TextInput/__tests__/SearchInput.test.tsx
+++ src/components/TextInput/__tests__/SearchInput.test.tsx
@@ -1,55 +1,57 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import TextInput from "../index";
+import TextInput from '../index';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../../styles/theme';
 
 const baseProps = {
   placeholder: 'Test placeholder search input',
   onChange: () => {},
-}
+};
 
 const renderWithTheme = (component: React.ReactNode) => {
   return render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
-}
+};
 
-test('Should render the search input without crashing', () => {
-  renderWithTheme(<TextInput {...baseProps} />);
-  const placeholder = screen.getByPlaceholderText(baseProps.placeholder);
-  expect(placeholder).toBeVisible();
-});
-
-test('Should trigger onChange when typing', async () => {
-  const user = userEvent.setup();
-  baseProps.onChange = jest.fn();
-
-  renderWithTheme(<TextInput {...baseProps} />);
-  const inputSearch = screen.getByPlaceholderText(baseProps.placeholder);
-  expect(inputSearch).toBeVisible();
-
-  await user.type(inputSearch, 'Teste value typed');
-
-  await waitFor(() => {
-    expect(baseProps.onChange).toBeCalled();
+describe('TextInput Component', () => {
+  it('should render the search input without crashing', () => {
+    renderWithTheme(<TextInput {...baseProps} />);
+    const placeholder = screen.getByPlaceholderText(baseProps.placeholder);
+    expect(placeholder).toBeVisible();
   });
 
-  // @ts-ignore
-  const event = baseProps.onChange.mock.calls[0][0];
-  expect(event.target.value).toBe('Teste value typed');
+  it('should trigger onChange when typing', async () => {
+    const user = userEvent.setup();
+    baseProps.onChange = jest.fn();
 
-  // @ts-ignore
-  expect(inputSearch.value).toBe('Teste value typed');
-});
+    renderWithTheme(<TextInput {...baseProps} />);
+    const inputSearch = screen.getByPlaceholderText(baseProps.placeholder);
+    expect(inputSearch).toBeVisible();
 
-test('Should have value provided for controlled behaviour', () => {
-  // @ts-ignore
-  baseProps.value = 'Test controlled';
-  renderWithTheme(<TextInput {...baseProps} />);
+    await user.type(inputSearch, 'Teste value typed');
 
-  const inputSearch = screen.getByPlaceholderText(baseProps.placeholder);
-  expect(inputSearch).toBeVisible();
+    await waitFor(() => {
+      expect(baseProps.onChange).toBeCalled();
+    });
 
-  // @ts-ignore
-  expect(inputSearch.value).toBe('Test controlled');
+    // @ts-ignore
+    const event = baseProps.onChange.mock.calls[0][0];
+    expect(event.target.value).toBe('Teste value typed');
+
+    // @ts-ignore
+    expect(inputSearch.value).toBe('Teste value typed');
+  });
+
+  it('should have value provided for controlled behaviour', () => {
+    // @ts-ignore
+    baseProps.value = 'Test controlled';
+    renderWithTheme(<TextInput {...baseProps} />);
+
+    const inputSearch = screen.getByPlaceholderText(baseProps.placeholder);
+    expect(inputSearch).toBeVisible();
+
+    // @ts-ignore
+    expect(inputSearch.value).toBe('Test controlled');
+  });
 });
--- src/components/TextInput/styles/index.tsx
+++ src/components/TextInput/styles/index.tsx
@@ -2,29 +2,29 @@ import styled from 'styled-components';
 import { BaseContainer } from '../../../styles/common';
 
 export const StyledTextBoxContainer = styled(BaseContainer)<{ width?: string }>`
-    display: flex;
-    width: ${props => props.width || 'auto'};
+  display: flex;
+  width: ${(props) => props.width || 'auto'};
 `;
 
 export const StyledTextBoxInput = styled.input<{ $hasIcon: boolean }>`
-    width: 100%;
-    border: none;
-    padding: 0;
-    box-sizing: border-box;
-    margin-left: ${props => props.$hasIcon ? props.theme.spacing.medium : '0'};
+  width: 100%;
+  border: none;
+  padding: 0;
+  box-sizing: border-box;
+  margin-left: ${(props) => (props.$hasIcon ? props.theme.spacing.medium : '0')};
 
-    &::placeholder {
-        font-family: ${({ theme }) => theme.fonts.primary};
-        font-size: ${({ theme }) => theme.fontSizes.medium};
-        font-weight: ${({ theme }) => theme.fontWeights.regular};
-        line-height: ${({ theme }) => theme.lineHeights.large};
-        text-align: left;
-        color: ${({ theme }) => theme.colors.textTertiary};
-    }
+  &::placeholder {
+    font-family: ${({ theme }) => theme.fonts.primary};
+    font-size: ${({ theme }) => theme.fontSizes.medium};
+    font-weight: ${({ theme }) => theme.fontWeights.regular};
+    line-height: ${({ theme }) => theme.lineHeights.large};
+    text-align: left;
+    color: ${({ theme }) => theme.colors.textTertiary};
+  }
 
-    :focus {
-        outline: none;
-        box-shadow: none;
-        border-color: transparent;
-    }
+  :focus {
+    outline: none;
+    box-shadow: none;
+    border-color: transparent;
+  }
 `;
