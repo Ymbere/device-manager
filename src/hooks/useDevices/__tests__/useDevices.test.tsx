9e239ab5b80e9623c43747c10e23a156b8386e3f
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 03:14:18 -0300

    chore: prettier write on useDevices

--- src/hooks/useDevices/__tests__/useDevices.test.tsx
+++ src/hooks/useDevices/__tests__/useDevices.test.tsx
@@ -1,40 +1,40 @@
 import React from 'react';
 import { renderHook, waitFor } from '@testing-library/react';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {useDevices} from "../index";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useDevices } from '../index';
 
 beforeEach(() => {
   jest.clearAllMocks();
-})
+});
 
-test('Should return the devices properly', async () => {
-  const queryClient = new QueryClient();
+describe('useDevices hook', () => {
+  it('should return the devices properly', async () => {
+    const queryClient = new QueryClient();
 
-  const wrapper = ({ children }: { children: React.ReactNode }) => (
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
-  )
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    );
 
-  const mockDevices = [
-    { id: '1', system_name: 'Device 1', type: 'Type A', hdd_capacity: '500' },
-    { id: '2', system_name: 'Device 2', type: 'Type B', hdd_capacity: '100' },
-  ];
+    const mockDevices = [
+      { id: '1', system_name: 'Device 1', type: 'Type A', hdd_capacity: '500' },
+      { id: '2', system_name: 'Device 2', type: 'Type B', hdd_capacity: '100' },
+    ];
 
-  global.fetch = jest.fn(() =>
+    global.fetch = jest.fn(() =>
       Promise.resolve({
         json: () => Promise.resolve(mockDevices),
       })
-  ) as jest.Mock;
+    ) as jest.Mock;
 
-  const { result } = renderHook(() => useDevices(), { wrapper });
+    const { result } = renderHook(() => useDevices(), { wrapper });
 
-  await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
 
-  const expectedDevices = mockDevices.map(device => ({
-    ...device,
-    hdd_capacity: Number(device.hdd_capacity),
-  }));
+    const expectedDevices = mockDevices.map((device) => ({
+      ...device,
+      hdd_capacity: Number(device.hdd_capacity),
+    }));
 
-  expect(result.current.data).toEqual(expectedDevices);
-})
+    expect(result.current.data).toEqual(expectedDevices);
+  });
+});
--- src/hooks/useDevices/index.ts
+++ src/hooks/useDevices/index.ts
@@ -1,28 +1,28 @@
-import {useQuery} from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { BASE_API_URL } from '../../consts';
 
 export interface Device {
-  id: string,
-  system_name: string,
-  type: string,
-  hdd_capacity: number,
+  id: string;
+  system_name: string;
+  type: string;
+  hdd_capacity: number;
 }
 
 const fetchDevices = async (): Promise<Array<Device>> => {
   const url = `${BASE_API_URL}/devices`;
   const response = await fetch(url);
 
-  const devices:Device[] = await response.json();
+  const devices: Device[] = await response.json();
 
-  return devices.map(device => ({
+  return devices.map((device) => ({
     ...device,
-    hdd_capacity: Number(device.hdd_capacity)
+    hdd_capacity: Number(device.hdd_capacity),
   }));
-}
+};
 
 export const useDevices = () => {
   return useQuery<Array<Device>, Error>({
     queryKey: ['devices'],
     queryFn: fetchDevices,
   });
-}
+};
