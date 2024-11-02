893d2b02b08e12530cd8e4a0a00fdced3d0ae628
Ymbere Xavier <ymbere@gmail.com>
2024-11-02 03:16:23 -0300

    chore: prettier write on WarningModal

--- src/pages/initial/modals/WarningModal.tsx
+++ src/pages/initial/modals/WarningModal.tsx
@@ -1,51 +1,51 @@
-import Modal from "../../../components/modal"
-import Button from "../../../components/button"
-import { useDeleteDevice } from "../../../hooks/useDeleteDevice";
-import { useDeviceDetail } from "../../../hooks/useDeviceDetail";
-import { ModalButtonText, ModalHeaderText } from "../../../styles/common";
+import Modal from '../../../components/modal';
+import Button from '../../../components/button';
+import { useDeleteDevice } from '../../../hooks/useDeleteDevice';
+import { useDeviceDetail } from '../../../hooks/useDeviceDetail';
+import { ModalButtonText, ModalHeaderText } from '../../../styles/common';
 
 interface WarningModalProps {
-    isOpen: boolean;
-    deviceId: string;
-    onClose: () => void;
+  isOpen: boolean;
+  deviceId: string;
+  onClose: () => void;
 }
 
 const WarningModal = ({ isOpen, deviceId, onClose }: WarningModalProps) => {
-    const { mutateAsync: deleteDevice } = useDeleteDevice();
-    const { data: deviceToDelete } = useDeviceDetail(deviceId);
+  const { mutateAsync: deleteDevice } = useDeleteDevice();
+  const { data: deviceToDelete } = useDeviceDetail(deviceId);
 
-    const handleDelete = async () => {
-        if (deviceId) {
-            await deleteDevice(deviceId);
-            onClose();
-        }
+  const handleDelete = async () => {
+    if (deviceId) {
+      await deleteDevice(deviceId);
+      onClose();
     }
+  };
 
-    return (
-        <Modal
-            modalTitle={(<ModalHeaderText>Delete device?</ModalHeaderText>)}
-            isOpen={isOpen}
-            onClose={onClose}
-            actions={(
-                <>
-                    <Button
-                        buttonLabel={(<ModalButtonText>Cancel</ModalButtonText>)}
-                        onClick={() => onClose()}
-                        buttonType='accessory'
-                    />
-                    <Button
-                        buttonLabel={(<ModalButtonText>Delete</ModalButtonText>)}
-                        onClick={() => handleDelete()}
-                        buttonType='danger'
-                    />
-                </>
-            )}
-        >
-            <div>
-                You are about to delete the device <strong>{deviceToDelete?.system_name}</strong>. This action cannot be undone.
-            </div>
-        </Modal>
-    )
-}
+  return (
+    <Modal
+      modalTitle={<ModalHeaderText>Delete device?</ModalHeaderText>}
+      isOpen={isOpen}
+      onClose={onClose}
+      actions={
+        <>
+          <Button
+            buttonLabel={<ModalButtonText>Cancel</ModalButtonText>}
+            onClick={() => onClose()}
+            buttonType="accessory"
+          />
+          <Button
+            buttonLabel={<ModalButtonText>Delete</ModalButtonText>}
+            onClick={() => handleDelete()}
+            buttonType="danger"
+          />
+        </>
+      }
+    >
+      <div>
+        You are about to delete the device <strong>{deviceToDelete?.system_name}</strong>. This action cannot be undone.
+      </div>
+    </Modal>
+  );
+};
 
 export default WarningModal;
